df574199c3607dd2e01543268c4863b6da00808e
Corey-G11 <86177801+Corey-G11@users.noreply.github.com>
2022-02-22 19:26:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+
 const generateTarget = () => {
   let secretNumber = Math.floor(Math.random()* 10);
   return secretNumber;
